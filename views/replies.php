bb1e46193b27deef909977a05749ca95ae8a2519
ClementGuerin <ticlem27270@gmail.com>
2018-06-06 23:20:47 +0200

    add SCSS

--- views/replies.php
+++ views/replies.php
@@ -1,19 +1,27 @@
-<div class="container">
-    <ul>
-        <?php if($reply1 !== null){ ?>
-        <li>
-            <button value="<?= htmlspecialchars($button1_story) ?>" id="button1" onclick="button(1);"><?= replaceApostrophe($button1_message) ?></button>
-        </li>
-        <?php } ?>
-        <?php if($reply2 !== null){ ?>
-        <li>
-            <button value="<?= htmlspecialchars($button2_story) ?>" id="button2" onclick="button(2);"><?= replaceApostrophe($button2_message) ?></button>
-        </li>
-        <?php } ?>
-        <?php if($reply3 !== null){ ?>
-        <li>
-            <button value="<?= htmlspecialchars($button3_story) ?>" id="button3" onclick="button(3);"><?= replaceApostrophe($button3_message) ?></button>
-        </li>
-        <?php } ?>
-    </ul>
+<div class="replies">
+    <div class="container">
+        <ul>
+            <?php if($reply1 !== null){ ?>
+            <li>
+                <button value="<?= htmlspecialchars($button1_story) ?>" id="button1" onclick="button(1);"><?= replaceApostrophe($button1_message) ?></button>
+            </li>
+            <?php } ?>
+            <?php if($reply2 !== null){ ?>
+            <li>
+                <button value="<?= htmlspecialchars($button2_story) ?>" id="button2" onclick="button(2);"><?= replaceApostrophe($button2_message) ?></button>
+            </li>
+            <?php } ?>
+            <?php if($reply3 !== null){ ?>
+            <li>
+                <button value="<?= htmlspecialchars($button3_story) ?>" id="button3" onclick="button(3);"><?= replaceApostrophe($button3_message) ?></button>
+            </li>
+            <?php } ?>
+            
+            <?php if($reply1 == null && $reply2 == null && $reply3 == null){ ?>
+            <li>
+                <button value="1" id="button0" onclick="button(0);">Recommencer...</button>
+            </li>
+            <?php } ?>
+        </ul>
+    </div>
 </div>
--- views/story.php
+++ views/story.php
@@ -1,9 +1,9 @@
-<div class="container">
-    <button value="1" id="button0" onclick="button(0);">Réinistialiser</button>
-    <input class="idStory" type="number" disabled value="<?= htmlspecialchars($idStory) ?>">
-    <?php if($messageStory !== null){ ?>
-    <p><?= replaceApostrophe($messageStory) ?></p>
-    <?php } else { ?>
-    <p>Message non trouvé</p>
-    <?php } ?>
+<div class="story">
+    <div class="container">
+        <?php if($messageStory !== null){ ?>
+        <p><?= replaceApostrophe($messageStory) ?></p>
+        <?php } else { ?>
+        <p>Message non trouvé</p>
+        <?php } ?>
+    </div>
 </div>
