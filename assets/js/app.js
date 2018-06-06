dbef041cb88e5a7a938c44ff4bc2886002482cb6
ClementGuerin <ticlem27270@gmail.com>
2018-06-07 00:26:53 +0200

    add localStorage progression

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,16 @@
 window.addEventListener('load', function(){
-    nextStory(1)
+    if(localStorage.getItem("currentStory") !== null){
+        nextStory(localStorage.getItem("currentStory"))
+    } else {
+        nextStory(1)
+        document.querySelector('.alert-save').style.display = "block";
+    }
 })
 
 function button(id){
     var thisStory = document.querySelector('#button' + id).value
+    localStorage.setItem("currentStory", thisStory)
+    document.querySelector('.alert-save').style.display = "";
     nextStory(thisStory)
 }
 
@@ -17,6 +24,17 @@ function nextStory(story){
     httpRequest.onreadystatechange = function(){
         if(httpRequest.readyState === 4){
             document.querySelector('.game').innerHTML = httpRequest.responseText
+            checkStory(story)
         }
+    }
+}
+
+function checkStory(story){
+    if(story == '31' || story == '44'){
+        document.querySelector('.story').style.color = "green"
+    } else if (story == '15' || story == '18' || story == '21' || story == '22' || story == '23' || story == '25' || story == '27' || story == '28' || story == '32' || story == '34' || story == '35' || story == '37' || story == '39' || story == '40' || story == '41' || story == '43'){
+        document.querySelector('.story').style.color = "red"
+    } else {
+        document.querySelector('.story').style.color = ""
     }
 }
--- index.php
+++ index.php
@@ -2,6 +2,14 @@
     include "includes/header.php";
 ?>
 
+<div class="container">
+    <div class="alert-save alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Hey salutation!</strong> Ton avancée dans l'histoire sera sauvegardé automatiquement, tu peux quitter cette page sans crainte tu retrouveras ta progression à ta reconnexion.
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+</div>
 <div class="game">
 </div>
 
